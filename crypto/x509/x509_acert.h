9ec88125af68176172f00c776b2d4d3de63eccfd
Damian Hobson-Garcia <dhobsong@igel.co.jp>
2021-05-13 11:53:59 +0900

    Add RFC 5755 attribute certificate support
    Add support for attribute certificates (v2) as described
    in RFC 5755 profile.
    
    Attribute certificates provide a mechanism to manage authorization
    information separately from the identity information provided by
    public key certificates.
    
    This initial patch adds the ASN.1 definitions
    and I/O API.  Accessor functions for the cetificate fields
    will be added in subsequent patches.

---
+++ crypto/x509/x509_acert.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2021 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OSSL_CRYPTO_X509_X509_ACERT_H
+# define OSSL_CRYPTO_X509_X509_ACERT_H
+
+# define X509_ACERT_ISSUER_V1 0
+# define X509_ACERT_ISSUER_V2 1
+#endif
